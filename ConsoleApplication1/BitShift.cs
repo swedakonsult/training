9283680657ba26055f68ece1993609eaeccac32e
Christer <christer@swedakonsult.com>
2014-02-14 10:23:57 -0800

    Addded tests that show that bit-shifting an integer can be tied to dividing the number.

---
+++ ConsoleApplication1/BitShift.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class BitShift
+    {
+        public static object ShiftRight(int pellet1, int shiftSize)
+        {
+            return pellet1 >> shiftSize;
+        }
+    }
+}
---
+++ ConsoleApplication1Tests/TestBitShift.cs
@@ -0,0 +1,68 @@
+﻿using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1Tests
+{
+    [TestClass]
+    public class TestBitShift
+    {
+        /// <summary>
+        /// Test showing that shifting to the right by 3 is the same as 
+        /// dividing an integer containing the result of 2 to the power of 3.
+        /// </summary>
+        [TestMethod]
+        public void TestShiftRight_ShiftBy3()
+        {
+            var guineaPig = new BitShift();
+            const int pellet1 = 345;
+            const int shiftSize = 3;
+            int divisor = (int)Math.Pow(2, shiftSize);
+
+            Assert.AreNotEqual(shiftSize, divisor, "Test setup error");
+
+            int truth = pellet1 / divisor;
+            Assert.AreEqual(truth, BitShift.ShiftRight(pellet1, shiftSize));
+        }
+
+        /// <summary>
+        /// Test showing that shifting to the right by 4 is the same as 
+        /// dividing an integer containing the result of 2 to the power of 4.
+        /// </summary>
+        [TestMethod]
+        public void TestShiftRight_ShiftBy4()
+        {
+            var guineaPig = new BitShift();
+            const int pellet1 = 345;
+            const int shiftSize = 4;
+            int divisor = (int)Math.Pow(2, shiftSize);
+
+            Assert.AreNotEqual(shiftSize, divisor, "Test setup error");
+
+            int truth = pellet1 / divisor;
+            Assert.AreEqual(truth, BitShift.ShiftRight(pellet1, shiftSize));
+        }
+
+        /// <summary>
+        /// Test showing that shifting to the right by 8 is the same as 
+        /// dividing an integer containing the result of 2 to the power of 8.
+        /// </summary>
+        [TestMethod]
+        public void TestShiftRight_ShiftBy8()
+        {
+            var guineaPig = new BitShift();
+            const int pellet1 = 34234225;
+            const int shiftSize = 8;
+            int divisor = (int)Math.Pow(2, shiftSize);
+
+            Assert.AreNotEqual(shiftSize, divisor, "Test setup error");
+
+            int truth = pellet1 / divisor;
+            Assert.AreEqual(truth, BitShift.ShiftRight(pellet1, shiftSize));
+        }
+    }
+}
